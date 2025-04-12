bc70c6e23dec2df4acf2e427aab844295549eeac
imusamadev <imusama.dev@gmail.com>
2025-04-13 03:09:25 +0500

    Crypto Currency Model Added

---
+++ CryptoPriceTrackerDemo/Models/CryptoCurrency.swift
@@ -0,0 +1,28 @@
+//
+//  CryptoCurrency.swift
+//  CryptoPriceTrackerDemo
+//
+//  Created by mac on 4/13/25.
+//
+
+import Foundation
+
+struct CryptoCurrency: Decodable, Identifiable {
+    let id: String
+    let symbol: String
+    let name: String
+    let currentPrice: Double
+    let marketCap: Double
+    let totalVolume: Double
+    let priceChangePercentage24h: Double
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case symbol
+        case name
+        case currentPrice = "current_price"
+        case marketCap = "market_cap"
+        case totalVolume = "total_volume"
+        case priceChangePercentage24h = "price_change_percentage_24h"
+    }
+}
